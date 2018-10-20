7519e013c7f73624ab30a9952b4a719c862c12ef
mannpy <maaz1313@gmail.com>
2018-10-20 21:14:01 +0500

    Добавлена секция 'Наши партнеры'

--- src/js/common.js
+++ src/js/common.js
@@ -24,6 +24,8 @@ $(document).ready(function(){
   $(".main-slider").slick({
     fade: true,
     dots: true,
+    autoplay: true,
+    autoplaySpeed: 6000,
     prevArrow: '<div class="slider-arrow slider-arrow_left"></div>',
     nextArrow: '<div class="slider-arrow slider-arrow_right"></div>',
   });
@@ -31,11 +33,47 @@ $(document).ready(function(){
   $(".slider-section").slick({
     prevArrow: '<div class="slider-arrow slider-arrow_left"></div>',
     nextArrow: '<div class="slider-arrow slider-arrow_right"></div>',
+    autoplay: true,
+    autoplaySpeed: 6000,
     responsive: [
     {
       breakpoint: 576,
       settings: {
         arrows: false,
+        dots: true,
+      }
+    },
+  ]
+  });
+
+  $(".partners-slider").slick({
+    slidesToShow: 5,
+    arrows: false,
+    responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 6000,
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+        autoplay: true,
+        autoplaySpeed: 6000,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 6000,
         dots: true,
       }
     },
